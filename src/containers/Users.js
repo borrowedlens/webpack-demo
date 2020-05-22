78323126e5e06fa732714c9af50c1d4ad887774e
Vivek <vivekprasad1410@gmail.com>
2020-05-22 21:55:34 +0530

    initial commit

---
+++ src/containers/Users.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Users extends Component {
+    render(){
+        return(
+            <div>
+                <p>This is a list of all the users!</p>
+            </div>
+        );
+    }
+};
+
+export default Users;
